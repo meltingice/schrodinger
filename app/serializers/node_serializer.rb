33e956e29caf1402e4b705fb69de87ce495d0600
Ryan LeFevre <meltingice8917@gmail.com>
2014-09-07 10:56:02 -0400

    Rebrand

--- app/serializers/node_serializer.rb
+++ app/serializers/node_serializer.rb
@@ -1,8 +1,8 @@
 class NodeSerializer < NodeNoAssociationSerializer
   attributes :category_stats
 
-  has_many :files,    serializer: NodeNoAssociationSerializer
-  has_many :folders,  serializer: NodeNoAssociationSerializer
+  # has_many :files,    serializer: NodeNoAssociationSerializer
+  # has_many :folders,  serializer: NodeNoAssociationSerializer
 
   def category_stats
     object.deep_categories_with_size
