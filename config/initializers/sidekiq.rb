b80d7c4b5f3a4328252ee9f4a533b1f1479792a2
Ryan LeFevre <meltingice8917@gmail.com>
2014-09-07 14:27:24 -0400

    Switch to REDIS_PROVIDER env

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,9 @@
-config = YAML.load(File.read(Rails.root.join('config/redis.yml')))[Rails.env.to_s]
-REDIS_PROVIDER ||= "redis://#{config['host']}:#{config['port']}"
-
 Sidekiq.configure_server do |config|
-  config.redis = { :url => REDIS_PROVIDER, :namespace => 'droplytics' }
+  config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'schrodinger' }
   config.default_worker_options = { 'queue' => Rails.env.to_s }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => REDIS_PROVIDER, :namespace => 'droplytics' }
+  config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'schrodinger' }
   config.default_worker_options = { 'queue' => Rails.env.to_s }
 end
