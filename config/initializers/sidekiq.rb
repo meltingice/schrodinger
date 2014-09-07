cacd3a6b99de106d56b6d951256c39c12b6db2dd
Ryan LeFevre <meltingice8917@gmail.com>
2014-09-07 14:30:16 -0400

    use REDISTOGO_URL instead

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'schrodinger' }
+  config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => 'schrodinger' }
   config.default_worker_options = { 'queue' => Rails.env.to_s }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { :url => ENV['REDIS_PROVIDER'], :namespace => 'schrodinger' }
+  config.redis = { :url => ENV['REDISTOGO_URL'], :namespace => 'schrodinger' }
   config.default_worker_options = { 'queue' => Rails.env.to_s }
 end
